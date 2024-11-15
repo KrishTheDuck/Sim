febac4668ceb3e4042006b27776c9ebd52b5f212
srikr <skrish18@terpmail.umd.edu>
2024-11-15 15:11:24 -0800

    Massive refactoring and restructuring.
    JPLQuery is now not a utility class.
    
    Plotter generates accurate times
    
    Mechanics added functions

--- Mechanics.py
+++ Mechanics.py
@@ -1,15 +1,17 @@
-import cmath
 import math
 
 import astropy
+import numpy as np
+import astropy.units as u
 from astropy.units import Quantity
 from hapsira.bodies import Earth
+from hapsira.twobody import Orbit
 from hapsira.util import norm
 
 
 class Mechanics:
     def __init__(self):
-        pass
+        raise "Utility Class Cannot be Instantiated."
 
     @staticmethod
     def hyperbolic_period(a: float, e: float, n1: float, n2: float) -> float:
@@ -41,7 +43,7 @@ class Mechanics:
 
         :param n2: Final true anomaly
         :param n1: Initial true anomaly
-        :param e: Eccentric
+        :param e: Eccentricity
         :param a: Semi-major axis
         :return: Total time for the transfer
         """
@@ -52,7 +54,27 @@ class Mechanics:
         E2 = math.atan(math.sin(n2) * math.sqrt(1 - e * e) / (math.cos(n2) + e))
         M2 = E2 - e * math.sin(E2)
 
-        return abs(math.sqrt(pow(a, 3) / Earth.k.value) * (M2 - M1))
+        print(M1, M2, " n1,n2 ", n1, n2)
+
+        # T^2 = pi a b sqrt(2/u * (1/r1 + 1/r2)
+        return abs(2 * math.sqrt(pow(a, 3) / Earth.k.value) * np.radians(n2 - n1))
+
+    @staticmethod
+    def elliptical_period(a: float, e: float, r1: float, r2: float):
+        b = a * math.sqrt(1 - e ** 2)
+        return 0.5 * math.pi * a * b * math.sqrt((2 / Earth.k.value) * (1 / r1 + 1 / r2))
+
+    @staticmethod
+    def vel_from_orbit(orb: Orbit, nu: float):
+        r, v = orb.rv()
+        r = r.to(u.m)
+        v = v.to(u.m / u.s)
+
+        h = np.linalg.norm(Mechanics.__cross(r.value, v.value))
+
+        r = (h ** 2 / Earth.k.value) / (1 + orb.ecc.value * np.cos(nu))
+
+        return Mechanics.vel(orb.a.value * 1000, r)
 
     @staticmethod
     def vel(a: float, R: float):
@@ -80,3 +102,73 @@ class Mechanics:
         unit_r = [-x / dir_norm for x in direction]
 
         return [(mag * x).value for x in unit_r]
+
+    @staticmethod
+    def __cross(A: np.ndarray, B: np.ndarray) -> np.ndarray:
+        C_x = A[1] * B[2] - A[2] * B[1]
+        C_y = A[2] * B[0] - A[0] * B[2]
+        C_z = A[0] * B[1] - A[1] * B[0]
+        return np.array([C_x, C_y, C_z])
+
+    @staticmethod
+    def orbital_elements(r, v, mu=398600.4418):  # mu is Earth's gravitational parameter in km^3/s^2
+        r = np.array(r)
+        v = np.array(v)
+
+        # Magnitude of position and velocity
+        r_norm = np.linalg.norm(r)
+        v_norm = np.linalg.norm(v)
+
+        # Angular momentum vector
+        h = Mechanics.__cross(r, v)
+        h_norm = np.linalg.norm(h)
+
+        # Inclination
+        inc = np.arccos(h[2] / h_norm)
+
+        # Node vector
+        n = Mechanics.__cross(np.array([0, 0, 1]), h)
+        n_norm = np.linalg.norm(n)
+
+        # Eccentricity vector
+        e = ((v_norm ** 2 - mu / r_norm) * r - np.dot(r, v) * v) / mu
+        ecc = np.linalg.norm(e)
+
+        # Right Ascension of the Ascending Node (RAAN)
+        if n_norm > 1e-10:  # Avoid division by zero
+            raan = np.arccos(n[0] / n_norm)
+            raan = 2 * np.pi - raan if n[1] < 0 else raan
+        else:
+            raan = 0.0
+
+        # Argument of Periapsis
+        if n_norm > 1e-10 and ecc > 1e-10:
+            arg_pe = np.arccos(np.dot(n, e) / (n_norm * ecc))
+            arg_pe = 2 * np.pi - arg_pe if e[2] < 0 else arg_pe
+        else:
+            arg_pe = 0.0
+
+        # True Anomaly
+        if ecc > 1e-10:
+            true_anomaly = np.arccos(np.dot(e, r) / (ecc * r_norm))
+            true_anomaly = 2 * np.pi - true_anomaly if np.dot(r, v) < 0 else true_anomaly
+        else:
+            cp = Mechanics.__cross(n, r)
+            true_anomaly = np.arccos(np.dot(n, r) / (n_norm * r_norm))
+            true_anomaly = 2 * np.pi - true_anomaly if cp[2] < 0 else true_anomaly
+
+        # Semi-major axis
+        a = 1 / (2 / r_norm - v_norm ** 2 / mu)
+
+        # Period (for elliptical orbits)
+        period = 2 * np.pi * np.sqrt(abs(a ** 3) / mu) if a > 0 else None
+
+        return {
+            "a": a,
+            "ecc": ecc,
+            "inc": np.degrees(inc),
+            "raan": np.degrees(raan),
+            "argp": np.degrees(arg_pe) + 180,
+            "nu": np.degrees(true_anomaly),
+            "T": period
+        }
--- Plotter.py
+++ Plotter.py
@@ -1,35 +1,33 @@
-from datetime import datetime
-
 import astropy.time
 import astropy.units as u
 import hapsira.frames
-import keyboard
 import numpy as np
-import pandas as pd
 from astropy.time import Time, TimeDelta
 from hapsira.bodies import Earth
-from hapsira.ephem import Ephem
+from hapsira.core.maneuver import (
+    bielliptic,
+)
 from hapsira.maneuver import Maneuver
 from hapsira.plotting import OrbitPlotter
 from hapsira.twobody import Orbit
-from hapsira.util import time_range
+from hapsira.util import time_range, norm
 from matplotlib import pyplot as plt
-from openpyxl.chart import LineChart, Reference
-from openpyxl.workbook import Workbook
 
 from JPLQuery import JPLQuery
 from Mechanics import Mechanics
 
 
 class Plotter:
+    rand_colors = ['red', 'green', 'blue', 'yellow', 'orange', 'violet']
+
     def __init__(self):
         pass
 
     @staticmethod
-    def __generate_orbit(ephemerides: dict, EPOCH: astropy.time.Time, r1: float, rb: float,
-                         plane: hapsira.frames.Planes, true_anomaly: float):
-        a = (r1 + rb) / 2.0
-        ecc = (r1 - rb) / (rb + r1)
+    def generate_orbit(ephemerides: dict, EPOCH: astropy.time.Time, r_min: float, r_max: float,
+                       plane: hapsira.frames.Planes, true_anomaly: float):
+        a = (r_min + r_max) / 2.0
+        ecc = (r_max - r_min) / (r_max + r_min)
 
         return Orbit.from_classical(
             attractor=Earth,
@@ -44,266 +42,92 @@ class Plotter:
         )
 
     @staticmethod
-    def plot_orbit(body, years, r1=10000.0 + Earth.R.value / 1000.0, show=False):
-        # Get the closest approach data (assuming JPLQuery is compatible with Hapsira)
-        EPOCH_NOW = Time("2024-11-2", scale='tdb')
-        Delta = TimeDelta(years * u.year, scale='tdb')
-        deltaV = []
-        deltaT = []
+    def plot_timed(body, start_time, end_time, delta, r1=100 * 1000.0 + Earth.R.value / 1000.0, show=False):
+        # Get the JPLQuery object located at the earth, starting from the initial query times given.
+        s = JPLQuery(body, "500@399", {'start': start_time, 'stop': end_time, 'step': '1d'})
+        # Zoom in to the closest approach and let the epochs
+        vals = s.zoom_to_closest_approach()
 
-        beg = EPOCH_NOW - Delta
-        end = EPOCH_NOW + Delta
+        hor = s.get_ephem(attractor=Earth,
+                          epoch=time_range(start=vals['date'] - delta,
+                                           end=vals['date'] + delta))
 
-        # Extract date and convert it to a suitable format
-        date_str = JPLQuery.get_closest_approach(body,
-                                                 beg.strftime("%Y-%m-%d"),
-                                                 end.strftime("%Y-%m-%d"))['date'] + ":00"
+        ephemerides = s.get_orbital_elements(
+            epochs_time_range=time_range(start=vals['date'] - delta,
+                                         end=vals['date'] + delta),
+            EPOCH=vals['date'])
 
-        date_obj = datetime.strptime(date_str, '%Y-%b-%d %H:%M:%S')
-        iso_format = date_obj.strftime('%Y-%m-%d %H:%M:%S')
+        # Precalculate the initial orbit
+        r_max = (vals['dist'] * u.km).to(u.m).value
+        r_min = (r1 * u.km).to(u.m).value
+        a = (r_min + r_max) / 2.0
+        e = (r_max - r_min) / (r_min + r_max)
 
-        # Set the epoch for the orbit
-        EPOCH = Time(iso_format, scale='tdb')
+        dv = abs(Mechanics.vel(r_min, r_min) - Mechanics.vel(a, r_min))
+        dt = TimeDelta(Mechanics.elliptical_period(a, e, r_min, r_max) * u.s)
 
-        # Earth.plot(EPOCH)
-        epochs = time_range(
-            start=EPOCH - TimeDelta(5 * u.day),
-            end=EPOCH + TimeDelta(5 * u.day)
+        # first transfer from MEO to asteroid
+        initial_orbit = Plotter.generate_orbit(ephemerides, vals['date'] - dt, r1, r1,
+                                               hor.plane, 0)
+        r, v = initial_orbit.rv()
+        v = v.to(u.m / u.s)
+        r = r.to(u.m)
+
+        rendezvous_orbit = initial_orbit.apply_maneuver(
+            Maneuver((0 * u.s, ((dv * v / norm(v)).value * (u.m / u.s))))
+        ).propagate(dt)
+
+        # Calculate dynamics of the return orbit
+        dv = abs(Mechanics.vel(r_min, r_min) - Mechanics.vel(a, r_max))
+
+        return_orbit = rendezvous_orbit.apply_maneuver(
+            Maneuver((0 * u.s, ((dv * r / norm(r)).value * (u.m / u.s))))
         )
 
-        # ephemerides query
-        orbit_epochs = {
-            'start': (EPOCH - TimeDelta(5 * u.day)).strftime('%Y-%m-%d 00:00:00'),
-            'stop': (EPOCH + TimeDelta(5 * u.day)).strftime('%Y-%m-%d 00:00:00'),
-            'step': '1h'
-        }
+        return_time = return_orbit.time_to_anomaly(return_orbit.nu + np.pi * u.rad)
+        return_orbit = return_orbit.propagate(return_time)
 
-        ephemerides = JPLQuery.get_ephemerides(body, "500@399", orbit_epochs, EPOCH)
-        orbital = Ephem.from_horizons(body, epochs, attractor=Earth)
+        # calculate adjustment orbit by computing a bi-elliptic transfer to a lower orbit.
+        adjustment_orbit = []
 
-        rb = (ephemerides['qr'] * 149597870.691) + Earth.R.value / 1000.0  # Convert AU to km
+        rv = return_orbit.rv()
+        rv = (rv[0].to_value(u.m), rv[-1].to_value(u.m / u.s))
 
-        orb_1 = Plotter.__generate_orbit(ephemerides, EPOCH, r1, r1, orbital.plane, np.pi / 2)
-        orb_2 = Plotter.__generate_orbit(ephemerides, EPOCH, r1, rb, orbital.plane, 0)
+        # first orbit arbitrarily can be 20 percent bigger than initial
+        dv_a, dv_b, dv_c, t_trans1, t_trans2 = bielliptic(Earth.k.value, r_min * 1.2, r_min, rv)
 
-        orb_1v = Mechanics.vel(1000 * r1, 1000 * r1)
-        orb_2v = Mechanics.vel(1000 * (r1 + rb) / 2, 1000 * r1)
+        man1 = Maneuver(
+            (0 * u.s, (dv_a * u.m / u.s).decompose())
+        )
 
-        deltaT.append(Mechanics.elliptic_period(1000 * (r1 + rb) / 2, orb_1.ecc, np.pi / 2, 0))
-        deltaV.append((orb_2v - orb_1v) / 1000 * (u.km / u.s))
+        adjustment_orbit.append(return_orbit.apply_maneuver(man1).propagate(t_trans1 * u.s + t_trans2 * u.s))
 
-        # set the orbit frame to be in plane with the earth and the asteroid
-        # frame.set_orbit_frame(orb_2)
-
-        # Orbit back
-        v_initial = Mechanics.vel(1000 * (r1 + rb) / 2, 1000 * rb)
-        v_final = Mechanics.vel(1000 * r1, 1000 * r1)
-
-        deltaV.append(abs(v_final - v_initial) / 1000 * (u.km / u.s))
-        dv_vector = Mechanics.vec_in_dir(deltaV[1].value, orb_2.r)
-
-        # TODO: fix instantaneous impulses and use engine thrust curves instead
-        man = Maneuver((0 * u.s, dv_vector * (u.km / u.s)))
-        orb_3 = orb_2.apply_maneuver(man)
-
-        print(orb_3.a.value, orb_3.ecc)
-        deltaT.append(Mechanics.hyperbolic_period(orb_3.a.value * 1000, orb_3.ecc, 0, -np.pi / 2))
-
-        # orb 4
-        man = Maneuver.hohmann(orb_3, r1 * u.km)
-        deltaV.append(man.get_total_cost())
-        orb_4 = orb_3.apply_maneuver(man)
+        # second little transfer
+        adjustment_orbit.append(adjustment_orbit[-1].apply_maneuver(
+            Maneuver.hohmann(adjustment_orbit[-1], r_min * u.m)
+        ))
 
         if show:
             frame = OrbitPlotter()
-            frame.set_attractor(Earth)
-            frame.plot(orb_1, label="Orbit 1", color="black")
-            frame.plot(orb_2, label="Orbit 2", color="orange")
-            frame.plot(orb_3, label="Orbit 3", color="blue")
-            frame.plot(orb_4, label="Orbit 4", color="green")
-            frame.plot_ephem(orbital, epoch=EPOCH, label=body, color="red")
+            frame.set_body_frame(Earth)
 
-            plt.xlim(-rb * 0.75, rb * 1.5)
-            plt.ylim(-rb, rb * 0.5)
+            frame.plot(initial_orbit, label="Initial Orbit", color="deepskyblue")
+            frame.plot(rendezvous_orbit, label="Rendezvous Orbit", color="darkorange")
+            frame.plot(return_orbit, label="Return Orbit", color="mediumseagreen")
+            frame.plot(adjustment_orbit[0], label="Adjustment Orbit", color="gold")
+            frame.plot(adjustment_orbit[1], label="Adjustment Orbit", color="gold")
 
-            plt.title(f"Hohmann Transfers for {body}")
+            frame.plot_ephem(hor,
+                             epoch=vals['date'], label=body,
+                             color='red')
+
+            plt.tight_layout()
             plt.show()
-
-        return deltaV, deltaT
-
-    @staticmethod
-    def plot_timed(body, start_time, end_time, r1=10 * 1000.0 + Earth.R.value / 1000.0, show=False):
-        # Transfer Cost, Transfer Time
-        deltaV = []
-        deltaT = []
-
-        query_epochs = {
-            'start': start_time.strftime('%Y-%m-%d 00:00:00'),
-            'stop': end_time.strftime('%Y-%m-%d 00:00:00'),
-            'step': '1d'
-        }
-
-        # Get closest approach data
-        data = JPLQuery.get_closest_approach(body,
-                                             "500@399",
-                                             query_epochs)
-        date_str = data['date'] + " 00:00:00"
-        date_obj = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
-        iso_format = date_obj.strftime('%Y-%m-%d %H:%M:%S')
-        EPOCH = Time(iso_format, scale='tdb')
-
-        # Calculate the Hohmann period. Get the Ephemerides of the body, and find the necessary orbital transfers.
-        rb = (data['distance_au'] * 149597870.691) + Earth.R.value / 1000.0  # Convert AU to km
-        deltaT.append(Mechanics.elliptic_period(1000 * (r1 + rb) / 2, 0, np.pi / 2, 0) * u.s)
-
-        print(f"rb from JPL {rb}")
-        # Plot the orbit of body so that it hits perigee at the end.
-        Delta = TimeDelta(deltaT[0], scale='tdb')
-
-        # Generate the orbital transfers.
-        query_epochs = {
-            'start': (EPOCH - Delta).strftime('%Y-%m-%d 00:00:00'),
-            'stop': EPOCH.strftime('%Y-%m-%d 00:00:00'),
-            'step': '1h'
-        }
-        orbital = Ephem.from_horizons(body, epochs=time_range(start=EPOCH - Delta, end=EPOCH), attractor=Earth)
-        ephemerides = JPLQuery.get_ephemerides(body, "500@399", query_epochs, EPOCH)
-
-        initial_orbit = Plotter.__generate_orbit(ephemerides, EPOCH, r1, r1, orbital.plane, np.pi / 2)
-        rendezvous_orbit = Plotter.__generate_orbit(ephemerides, EPOCH, r1, rb, orbital.plane, 0)
-
-        # Return Orbit Transfer requires the same cost
-        v_initial = Mechanics.vel(1000 * r1, 1000 * r1)
-        v_rendezvous = Mechanics.vel(1000 * (r1 + rb) / 2, 1000 * r1)
-        v_return = Mechanics.vel(1000 * (r1 + rb) / 2, 1000 * rb)
-
-        dv_vector = Mechanics.vec_in_dir(abs(v_initial - v_return) / 1000 * (u.km / u.s), rendezvous_orbit.r)
-        return_orbit = rendezvous_orbit.apply_maneuver(Maneuver((0 * u.s, dv_vector * (u.km / u.s))))
-
-        deltaV.append((v_rendezvous - v_initial) / 1000 * (u.km / u.s))
-        deltaV.append(abs(v_return - v_initial) / 1000 * (u.km / u.s))
-        deltaT.append(Mechanics.hyperbolic_period(return_orbit.a.value * 1000, return_orbit.ecc, 0, -np.pi / 2) * u.s)
-
-        # Adjustment
-        man = Maneuver.hohmann(return_orbit, r1 * u.km)
-        deltaV.append(man.get_total_cost())
-        deltaT.append(man.get_total_time())
-        adjustment_orbit = return_orbit.apply_maneuver(man)
-
-        if show:
-            frame = OrbitPlotter()
-            frame.set_attractor(Earth)
-
-            frame.plot(initial_orbit, label="Initial Orbit", color="blue")
-            frame.plot(rendezvous_orbit, label="Rendezvous Orbit", color="orange")
-            frame.plot(return_orbit, label="Return Orbit", color="green")
-            frame.plot(adjustment_orbit, label="Adjustment", color="yellow")
-            frame.plot_ephem(orbital, epoch=EPOCH, label=body, color="red")
-
-            plt.title(f"Hohmann Transfers for {body}")
-            plt.show()
-
-        return deltaV, deltaT
-
-
-def to_excel(col_data, col_titles, path, title, x_axis_title, y_axis_title):
-    # Create a DataFrame from the provided column data and titles
-    data_frame_args = {col_titles[index]: col_data[index] for index in range(len(col_titles))}
-    data = pd.DataFrame(data_frame_args)
-
-    # Save the DataFrame to an Excel file
-    data.to_excel(path, sheet_name=ast_name, index=False)
-
-    # Open the existing workbook (instead of creating a new one)
-    wb = Workbook()
-    ws = wb.active
-
-    ws.append(col_titles)
-    for r in data.itertuples(index=False):
-        ws.append(r)
-
-    # Create a LineChart object
-    chart = LineChart()
-
-    # Set chart title and axis labels
-    chart.title = title
-    chart.x_axis.title = x_axis_title
-    chart.y_axis.title = y_axis_title
-
-    # Define data reference for Y-axis (data range)
-    data_ref = Reference(ws, min_col=5, min_row=2, max_row=ws.max_row)  # Assuming data starts from column 2
-
-    # Define categories (X-axis labels)
-    categories_ref = Reference(ws, min_col=1, min_row=2, max_row=ws.max_row)  # Assuming X-axis is in column 1
-
-    # Add data and categories to the chart
-    chart.add_data(data_ref, titles_from_data=True)
-    chart.set_categories(categories_ref)
-
-    # Add the chart to the worksheet at a specific position
-    ws.add_chart(chart, "G5")
-
-    # Save the workbook with the chart added
-    wb.save(path)
 
 
 if __name__ == "__main__":
-    def get_sub_l(list_of_lists, index):
-        return list(map(lambda values: values[index].value, list_of_lists))
-
-
-    ast_name = "2024 PT5"
-    vels = []
-    times = []
-
-    now = Time("2024-11-2", scale='tdb')
-    time_delta = TimeDelta(10 * u.year, scale='tdb')
-
-    beg = now - time_delta
-    end = now + time_delta
-
-    x = range(10, 101)
-    for i in x:
-        res = Plotter.plot_timed(ast_name, beg, end, r1=i * 1000.0 + Earth.R.value / 1000.0, show=True)
-        print(res)
-        vels.append(res[0])
-        times.append(sum(res[1]).value / (3600 * 24))
-
-        input("continue...")
-        if keyboard.is_pressed('q'):
-            break
-
-    magnitudes = [sum(x).value for x in vels]
-
-    to_excel([list(x), get_sub_l(vels, 0), get_sub_l(vels, 1), get_sub_l(vels, 2), magnitudes, times],
-             ["x", "V1", "V2", "V3", "|V|, T"],
-             'data.xlsx',
-             'Cost vs Initial LEO Orbit',
-             'Initial Orbital Distance (1e3 km)',
-             'Total Cost (km/s)')
-
-    # Create the figure and axis
-    fig, ax1 = plt.subplots()
-
-    # Plot the total cost on the primary y-axis (left)
-    ax1.plot(list(x), magnitudes, 'b-', label='Total Cost')
-
-    ax1.set_xlabel('Initial Orbital Radius (1000 km)')
-    ax1.set_ylabel('Total Cost (km/s)', color='b')
-    ax1.tick_params(axis='y', labelcolor='b')
-    ax1.legend()
-
-    # Create a secondary y-axis for time in days
-    ax2 = ax1.twinx()
-
-    ax2.plot(list(x), times, 'r-', label='Time of Flight')
-    ax2.set_ylabel('Time of Flight (days)', color='r')
-    ax2.tick_params(axis='y', labelcolor='r')
-    ax2.legend()
-
-    # Add title and show the plot
-    plt.title('Total Cost vs Initial Orbital Radius with Time of Flight')
-
-    plt.legend()
-    fig.tight_layout()  # Adjust layout to prevent overlap
-    plt.show()
+    Plotter.plot_timed("2006 WB",
+                       Time.now().strftime("%Y-%m-%d 00:00:00"),
+                       (Time.now() + TimeDelta(1 * u.year)).strftime("%Y-%m-%d 00:00:00"),
+                       0.5 * u.day,
+                       show=True)
